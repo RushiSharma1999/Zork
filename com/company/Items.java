4fa2bcf68b5b6d287a53ce5409d3c8da10dfff81
SuperSpicyCatfish <julia4reals@icloud.com>
2020-10-16 13:21:10 -0700

    Update Items.java
    - Weapons class updated
    - Items interface was updated

--- com/company/Items.java
+++ com/company/Items.java
@@ -1,5 +1,5 @@
 //
-//	AUTHORS:	 	Julia Real, Tran, and Rushi
+//	AUTHORS:	 	Julia, Tran, and Rushi
 //	CLASS: 			CPSC - 233j
 //	TEACHER:		Professor McCarthy
 //	TIME:			T-Th 8:00am-9:45am
@@ -17,6 +17,7 @@ interface Items {
 
 	public String getName();				// returns the name of an item
 	public String getDescription();			// returns the description of an item
+
 	public void useItem();					// each class will provide functionality
 	public boolean canPickUp();  			// returns true/false if the player can pick up this item
 }
@@ -27,18 +28,45 @@ interface Items {
 class Weapons implements Items {
 	String name;
 	String description;
-	int damage;								// we can possible split this into different Damage types ie: bleed, piercing, severing....
-	boolean pickUp = true;
-	public String getName(){		
-		return name;
+	int damage;						// we can possible split this into different Damage types ie: bleed, piercing, severing....
+	boolean pickUp;					
+
+
+
+	public Weapons(){				// Telescoping constructors, I am selective on how many constructors im making :p
+		this("UNKNOWN NAME","NO DESCRIPTION", 0, false);		
 	}
 
-	public String getDescription(){
+	public Weapons(String name, String description, int damage, boolean pickUp){
+		this.name = name;
+		this.description = description;
+		this.damage = damage;
+		this.pickUp = pickUp;
+	}
+
+	public void setName(String name){ 					// setter function
+		this.name = name;
+	}
+
+	public String getName(){							// getter function
+		return name;	
+	}
+
+	public void setDescription(String description){	 	// setter function
+		this.description = description;
+	}
+
+	public String getDescription(){	 					// getter function
 		return description;
 	}
 
-	public void useItem(){					// Weapon's useItem() will attack and do damage to a valid target
-		System.out.println("You ATTACK with " + name);		
+	public boolean canPickUp(){							// setter function
+		return pickUp;			
+	}
+
+	public void useItem(){								// Weapon's useItem() will attack and do damage to a valid target
+		System.out.println("You ATTACK with " + name);	
+		System.out.println("You did " + damage + " damage");	
 
 		// Check if valid target
 		// If target is a Character, lower health/do damage
@@ -46,9 +74,10 @@ class Weapons implements Items {
 
 	}
 
-	public boolean canPickUp(){
-		return pickUp;
-	}
+	// public String toString(){
+		
+	// }
 
+	
 }
 
