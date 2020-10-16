dfa2ad0a174a899720536220d46d63be50199c93
SuperSpicyCatfish <julia4reals@icloud.com>
2020-10-16 11:55:57 -0700

    Updated Items.java
    - Item interface
    - Weapon class

--- com/company/Items.java
+++ com/company/Items.java
@@ -7,3 +7,48 @@
 //
 
 package com.company;
+
+
+
+//=====================================================================
+// Item interface
+//=====================================================================
+interface Items {
+
+	public String getName();				// returns the name of an item
+	public String getDescription();			// returns the description of an item
+	public void useItem();					// each class will provide functionality
+	public boolean canPickUp();  			// returns true/false if the player can pick up this item
+}
+
+//=====================================================================
+// Weapon class
+//=====================================================================
+class Weapons implements Items {
+	String name;
+	String description;
+	int damage;								// we can possible split this into different Damage types ie: bleed, piercing, severing....
+	boolean pickUp = true;
+	public String getName(){		
+		return name;
+	}
+
+	public String getDescription(){
+		return description;
+	}
+
+	public void useItem(){					// Weapon's useItem() will attack and do damage to a valid target
+		System.out.println("You ATTACK with " + name);		
+
+		// Check if valid target
+		// If target is a Character, lower health/do damage
+		// else the attack does nothing? possible implement unique interactions ie: a axe breaks a door
+
+	}
+
+	public boolean canPickUp(){
+		return pickUp;
+	}
+
+}
+
